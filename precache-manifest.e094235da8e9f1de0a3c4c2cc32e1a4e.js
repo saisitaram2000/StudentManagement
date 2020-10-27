1855413e3733aaca25a683b0fc8225466d544fbc
VADDAVALLI SAI SITA RAM <saisitaramvaddavalli@gmail.com>
2020-10-27 13:46:34 +0530

    Updates

---
+++ precache-manifest.e094235da8e9f1de0a3c4c2cc32e1a4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edafe9630875180a1bfc9d0491f47363",
+    "url": "/StudentManagement/index.html"
+  },
+  {
+    "revision": "94700b508d59139e4167",
+    "url": "/StudentManagement/static/css/main.90700b37.chunk.css"
+  },
+  {
+    "revision": "285fb9b09964e2a3a1cb",
+    "url": "/StudentManagement/static/js/2.c386e898.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/StudentManagement/static/js/2.c386e898.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94700b508d59139e4167",
+    "url": "/StudentManagement/static/js/main.ec4958ea.chunk.js"
+  },
+  {
+    "revision": "d1206da9571c0ab0810d",
+    "url": "/StudentManagement/static/js/runtime-main.9c9cda46.js"
+  }
+]);
