5102f121299e15fe956b5d7486bea902651372c9
VADDAVALLI SAI SITA RAM <saisitaramvaddavalli@gmail.com>
2020-10-27 19:54:37 +0530

    Updates

---
+++ precache-manifest.52256ef2d6e808b34e819535443fddb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88ca2d1158a796e4974182cf9a5ff917",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c693d91f72bf69aba96",
+    "url": "/static/css/main.90700b37.chunk.css"
+  },
+  {
+    "revision": "cca4ba30f23392564ebd",
+    "url": "/static/js/2.962ddd68.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.962ddd68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c693d91f72bf69aba96",
+    "url": "/static/js/main.3839ad38.chunk.js"
+  },
+  {
+    "revision": "68a9665f6992de0c4239",
+    "url": "/static/js/runtime-main.fc75219d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/StudentManagement/precache-manifest.d8ff8168f91c109ca95ce5a053908789.js"
+  "/precache-manifest.52256ef2d6e808b34e819535443fddb8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/StudentManagement/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
